ead5ce7178f6cf143b14cbe47eabb024ece7ea82
crasher <geritix@mail.ru>
2021-02-11 02:50:32 +0300

    Implemented calculating company rank and added callback for slug value

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,14 @@
 class Company < ApplicationRecord
   has_many :reviews
+
+  before_create :set_slug
+
+  def set_slug
+    self.slug = self.name.parameterize
+  end
+
+  def calculate_rank(precision: 2)
+    avg_rank = reviews.average(:rank)
+    avg_rank.round(precision).to_f
+  end
 end
