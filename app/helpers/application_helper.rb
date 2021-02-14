be4dedaa8fb37c70193a42fe7be7a33784841eff
crasher <geritix@mail.ru>
2021-02-14 04:00:56 +0300

    Changed logic related to serialize method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
   def serialize(object, serializer_class, options = nil, type = :json)
-    serializer = options ? serializer_class.new(object, options) : serializer_class.new(object)
+    serializer = create_serializer(object, serializer_class, options)
     if type == :hash
       serializer.serializable_hash
     else
       serializer.serialized_json
+    end
+  end
+
+  def create_serializer(object, serializer_class, options = nil)
+    if options
+      serializer_class.new(object, options)
+    else
+      serializer_class.new(object)
     end
   end
 end
