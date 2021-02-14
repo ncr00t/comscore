7be1afd1c5e84c487646f508fc92ea2a2314db8c
crasher <geritix@mail.ru>
2021-02-14 03:58:21 +0300

    Implemented API methods for reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,30 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
+
+      def create
+        review = Review.new(review_params)
+        if review.save
+          render json: serialize(review, ReviewSerializer)
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = Review.find(params[:id])
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :rank, :company_id)
+      end
     end
   end
 end
